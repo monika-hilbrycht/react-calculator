1eed0fdd1efbdfeaee7a7b58e7ebd9c19f6e8fa2
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-09 11:25:25 +0100

    calculator structure

---
+++ calc/src/components/Main.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function Main()
+
+{
+
+    return (
+        <div className='row justify-content-center'>
+
+            <div className="col-md-5">
+
+                <h1>React Calc</h1>
+
+                <table className='table'>
+                    <tr>
+                        <input type="text"></input>
+                        <button>C</button>
+                    </tr>
+
+                    <tr>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>/</button>
+                    </tr>
+                    <tr>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>-</button>
+                    </tr>
+                    <tr>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>+</button>
+                    </tr>
+                    <tr>
+                        <button>.</button>
+                        <button>0</button>
+                        <button>=</button>
+                        <button>*</button>
+                    </tr>
+
+                </table>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Main;
