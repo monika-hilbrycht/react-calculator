2b62e1f125a2dfbcd4d103539bccd04820597bc9
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2023-01-09 11:40:50 +0100

    style

--- calc/src/components/Main.js
+++ calc/src/components/Main.js
@@ -7,39 +7,39 @@ function Main()
     return (
         <div className='row justify-content-center'>
 
-            <div className="col-md-5">
+            <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
 
                 <h1>React Calc</h1>
 
                 <table className='table'>
                     <tr>
-                        <input type="text"></input>
-                        <button>C</button>
+                       <td colSpan='3'><input type="text"></input></td>
+                        <td><button>C</button></td>
                     </tr>
 
                     <tr>
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>/</button>
+                    <td><button>1</button></td>
+                    <td><button>2</button></td>
+                    <td><button>3</button></td>
+                    <td><button>/</button></td>
                     </tr>
                     <tr>
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button>-</button>
+                    <td><button>4</button></td>
+                    <td><button>5</button></td>
+                    <td><button>6</button></td>
+                    <td><button>-</button></td>
                     </tr>
                     <tr>
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button>+</button>
+                    <td><button>7</button></td>
+                    <td><button>8</button></td>
+                    <td><button>9</button></td>
+                    <td><button>+</button></td>
                     </tr>
                     <tr>
-                        <button>.</button>
-                        <button>0</button>
-                        <button>=</button>
-                        <button>*</button>
+                    <td><button>.</button></td>
+                    <td><button>0</button></td>
+                    <td><button>=</button></td>
+                    <td><button>*</button></td>
                     </tr>
 
                 </table>
